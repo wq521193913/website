25b2267df418d457a1635c8d1656178851d57a33
1 <1@qq.com>
2016-05-16 17:24:10 +0800

    source commit

---
+++ website/js/config.route.js
@@ -0,0 +1,10 @@
+/**
+ * Created by aa wq 2016/4/29.
+ */
+var routes = {
+    prefix:'view/',
+    pageList:[
+        {'serf':'app','url':'/app','templateUrl':'app.html','controller':''},
+        {'serf':'app.product','url':'/product','templateUrl':'product.html','controller':'','lzLoadJs':''}//frontProduct //view/front.product.js
+    ]
+}
